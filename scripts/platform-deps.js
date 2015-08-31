ac3347307d65d60ea62bf343ca2afec1807f4b64
Michel Boudreau <michelb@pageuppeople.com>
2015-08-31 17:25:42 +1000

    changing the way the os based install is added

--- scripts/platform-deps.js
+++ scripts/platform-deps.js
@@ -1,19 +1,20 @@
 //Download and install the correct platform specific package for the mock server
-var npm = require("npm");
+var npm = require("npm"),
+	exec = require('child_process').exec;
 var arch = "";
-npm.load(function(er, npm) {
+npm.load(function (er, npm) {
+	if (process.platform === 'linux') {
+		arch = '-' + process.arch;
+	}
+	var packageName = 'pact-mock-service-' + process.platform + arch;
+	console.log("Installing Pact mock server for " + packageName);
 
-  if (process.platform === 'linux') {
-    arch = '-' + process.arch;
-  }
-  console.log("Installing Pact mock server for " + process.platform + arch);
-
-  //Install npm into the wrapping pact-mock-service node_modules directory to avoid nested node_modules
-  npm.commands.install("../../", ['pact-mock-service-' + process.platform + arch], function(er, data) {
-    if(er) {
-      console.log(er);
-      process.exit(1);
-    }
-    console.log("Pact mock server for - " + process.platform + arch + " installed successfully.");
-  });
-})
+	//Install npm into the wrapping pact-mock-service node_modules directory to avoid nested node_modules
+	exec('npm install ' + packageName + '@latest', function (error, stdout, stderr) {
+		if (error) {
+			console.error(error);
+			process.exit(1);
+		}
+		console.log("Pact mock server for - " + packageName + " installed successfully.");
+	});
+});
