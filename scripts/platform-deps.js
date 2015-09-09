8d711717c8bbf963738115c9ffacb50698dd7c0f
Michel Boudreau <michelb@pageuppeople.com>
2015-09-09 18:05:11 +1000

    updating package version to be the same as the standalone packages, make sure that it installs the same version across

--- scripts/platform-deps.js
+++ scripts/platform-deps.js
@@ -16,24 +16,21 @@ npm.load(function (er, npm) {
 	console.log("Installing Pact mock server for " + packageName);
 
 	//Install npm into the wrapping pact-mock-service node_modules directory to avoid nested node_modules
-	exec('npm install ' + packageName + '@latest', {cwd: path.resolve(__dirname, '..')}, function (error, stdout, stderr) {
+	exec('npm install ' + packageName + '@'+pkg.version, {cwd: path.resolve(__dirname, '..')}, function (error, stdout, stderr) {
 		if (error) {
 			console.error(error);
 			process.exit(1);
 		}
-		console.log("Pact mock server for - " + packageName + " installed successfully.");
+		console.log("Pact mock server for " + packageName + " installed successfully.");
 
 		// Specify the bin path of the downloaded package, in the bin path of pact-mock-service
 		var p = path.resolve(__dirname, '../node_modules/' + packageName);
 		var bin = require(path.join(p, 'package.json')).bin;
-		bin['pact-mock-service'] = './'+ path.relative(path.dirname(pkgPath), path.resolve(p, bin['pact-mock-service'])); // Setting path to be relative to project's package.json
+		// Setting path to be relative to project's package.json
+		bin['pact-mock-service'] = './'+ path.relative(path.dirname(pkgPath), path.resolve(p, bin['pact-mock-service']));
 		pkg.bin = bin;
 
 		// Write it to package.json
 		fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
 	});
-
-	// TODO: remove view to get latest version, use the same version as 'pkg'
-	// TODO: instead of installing new package, download the latest and extract bin & lib into main folder
-	// TODO: change package.json bin path based on package.json of arch specific package
 });
